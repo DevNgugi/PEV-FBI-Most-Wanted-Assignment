d2d8fe677099ce726cafeca673659547a2cb95f0
DevNgugi <philemonngugi18@gmail.com>
2025-07-25 18:14:55 +0300

    added prettier fos, added logging

--- src/controllers/wanted.controller.js
+++ src/controllers/wanted.controller.js
@@ -1,12 +1,14 @@
 const { fetchWantedList } = require('../services/wanted.service');
+const logger = require('../utils/logger');
 
 async function getWantedList(req, res) {
   try {
     const { page } = req.query;
     const data = await fetchWantedList({ page });
+
     res.json({ success: true, data });
   } catch (err) {
-    console.error(err.message);
+    logger.error(`${req.ip} ${req.method} ${req.originalUrl} - Error: ${err.message}`);
     res.status(500).json({ success: false, message: 'Internal server error' });
   }
 }
--- src/services/wanted.service.js
+++ src/services/wanted.service.js
@@ -1,19 +1,22 @@
 const createHttpClient = require('../utils/apiClient');
 const redisHelper = require('../utils/redisHelper');
+const logger = require('../utils/logger');
 
-const API_URL =  `${process.env.FBI_API_BASE_URL}/list`;
+const API_URL = `${process.env.FBI_API_BASE_URL}/list`;
 
 const api = createHttpClient(API_URL);
 async function fetchWantedList({ page = 1 }) {
   const params = {
-    page
+    page,
   };
-
-   return redisHelper('list', params, async () => {
-    const response =  await api.get(API_URL, { params });
-    return response.data;
-  });
-  
+  try {
+    return redisHelper('list', params, async () => {
+      const response = await api.get(API_URL, { params });
+      return response.data;
+    });
+  } catch (error) {
+    logger.error(`FBI fetch failed: ${error.message}`);
+  }
 }
 
 module.exports = { fetchWantedList };
